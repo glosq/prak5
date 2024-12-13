01a4a26759487c1f46287fabfd62ef57eb12aee9
ИСП-34 <-34@LAB17-10>
2024-12-13 16:25:50 +0300

    Добавьте файлы проекта.

---
+++ Pair/Class1.cs
@@ -0,0 +1,19 @@
+﻿namespace Pair
+{
+    public class Piar
+    {
+        public int value1 { get; set; }
+        public int value2 { get; set; }
+        public int Prais ()
+        {
+            return value1 * value2;
+        }
+        public Piar Prais ( Piar CL2)
+        {
+            Piar p = new Piar ();
+            p.value1= this .value1*CL2.value1;
+            p.value2= this .value2*CL2.value2;
+            return p;
+        }
+    }
+}
---
+++ prak5/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Pair;
+
+namespace prak5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Oprog_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Выполнил \n Жаров Артём \n ИСП-34");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Piar par = new Piar();//оглавляешь переменную класса ПАИР
+            par.value1=0;               //парсишь значения
+            par.value2=0;               //парсишь значения
+            Piar par2 = new Piar();//оглавляешь переменную класса ПАИР
+            par2.value1 = 0;             //парсишь значения
+            par2.value2 = 0;             //парсишь значения
+            int a = par.Prais();        //находишь произведение внутри 1 пары
+            Piar aa = par.Prais(par2);//находишь произведение между первой и второй 
+            
+
+        }
+    }
+}
